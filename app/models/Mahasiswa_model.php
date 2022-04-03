ea687d976af4ef56d90dea668b404ae838330900
NicholasAli <nicholas.ali606@gmail.com>
2022-04-03 19:30:34 +0700

    insiasi

---
+++ app/models/Mahasiswa_model.php
@@ -0,0 +1,15 @@
+<?php
+
+class Mahasiswa_model {
+    private $table = 'mahasiswa';
+    private $db; 
+
+    public function __construct() {
+        $this->db = new Database;
+    }
+
+    public function getAllMahasiswa() {
+        $this->db->query('SELECT * FROM' . $this->table);
+        return $this->db->resultSet();
+    }
+}
---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL ?>/img/profile.jpg" alt=""ucup" width="200" class="rounded-circle">
+    <p>Halo, nama saya <?= $data['nama']?>, umur saya <?= $data['umur']?>, saya seorang <?= $data['pekerjaan']?></p>
+    </div>
